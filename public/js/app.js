78093770e6d796f9d8cf95d89f8d68a4033dcb9e
Georgi Daskalov <georgi.daskalov@mentormate.com>
2020-03-20 10:48:51 +0200

    Section 8 Lesson 109

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 const Logger = (logString) => {
     return (constructor) => {
         console.log(logString);
@@ -33,3 +36,49 @@ Person = __decorate([
 ], Person);
 const person = new Person();
 console.log("person", person);
+const Log = (target, propertyName) => {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+};
+const Log2 = (target, propertyName, descriptor) => {
+    console.log('Accessor decorator!');
+    console.log('target', target);
+    console.log('propertyName', propertyName);
+    console.log('descriptor', descriptor);
+};
+const Log3 = (target, name, descriptor) => {
+    console.log('Method decorator!');
+    console.log('target', target);
+    console.log('name', name);
+    console.log('descriptor', descriptor);
+};
+const Log4 = (target, name, position) => {
+    console.log('Parameter decorator!');
+    console.log('target', target);
+    console.log('name', name);
+    console.log('position', position);
+};
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
--- src/app.ts
+++ src/app.ts
@@ -27,4 +27,53 @@ class Person {
 }
 
 const person = new Person();
-console.log("person", person)
+console.log("person", person);
+
+const Log = (target: any, propertyName: string | Symbol) => {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+const Log2 = (target: any, propertyName: string | Symbol, descriptor: PropertyDescriptor) => {
+    console.log('Accessor decorator!');
+    console.log('target', target);
+    console.log('propertyName', propertyName);
+    console.log('descriptor', descriptor);
+}
+
+const Log3 = (target: any, name: string | Symbol, descriptor: PropertyDescriptor) => {
+    console.log('Method decorator!');
+    console.log('target', target);
+    console.log('name', name);
+    console.log('descriptor', descriptor);
+}
+
+const Log4 = (target: any, name: string | Symbol, position: number) => {
+    console.log('Parameter decorator!');
+    console.log('target', target);
+    console.log('name', name);
+    console.log('position', position);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        }
+    }
+
+    @Log3
+    getPriceWithTax (@Log4 tax: number) {
+        return this._price * (1 + tax);
+    }
+}
