26ce07b6212c976787682d0408d86f9223011edb
Georgi Daskalov <georgi.daskalov@mentormate.com>
2020-03-20 11:40:20 +0200

    Section 8 Lesson 113

--- public/js/app.js
+++ public/js/app.js
@@ -5,82 +5,30 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-const Logger = (logString) => {
-    return (constructor) => {
-        console.log(logString);
-        console.log(constructor);
+const AutoBind = (_, _2, descriptor) => {
+    console.log('descriptor', descriptor);
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFunction = originalMethod.bind(this);
+            return boundFunction;
+        },
     };
+    return adjustedDescriptor;
 };
-const WithTemplate = (template, hookId) => {
-    return (originalConstructor) => {
-        return class extends originalConstructor {
-            constructor(..._) {
-                super();
-                const element = document.getElementById(hookId);
-                if (element) {
-                    element.innerHTML = template;
-                    element.querySelector('h1').textContent = this.name;
-                }
-            }
-        };
-    };
-};
-let Person = class Person {
+class Printer {
     constructor() {
-        this.name = 'JD';
-        console.log('Started!');
+        this.message = 'This works!';
     }
-};
-Person = __decorate([
-    Logger('Logging'),
-    WithTemplate('<h1>My person object</h1>', 'app')
-], Person);
-const Log = (target, propertyName) => {
-    console.log('Property decorator!');
-    console.log(target, propertyName);
-};
-const Log2 = (target, propertyName, descriptor) => {
-    console.log('Accessor decorator!');
-    console.log('target', target);
-    console.log('propertyName', propertyName);
-    console.log('descriptor', descriptor);
-};
-const Log3 = (target, name, descriptor) => {
-    console.log('Method decorator!');
-    console.log('target', target);
-    console.log('name', name);
-    console.log('descriptor', descriptor);
-};
-const Log4 = (target, name, position) => {
-    console.log('Parameter decorator!');
-    console.log('target', target);
-    console.log('name', name);
-    console.log('position', position);
-};
-class Product {
-    constructor(t, p) {
-        this.title = t;
-        this._price = p;
-    }
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-    }
-    getPriceWithTax(tax) {
-        return this._price * (1 + tax);
+    showMessage() {
+        console.log(this.message);
     }
 }
 __decorate([
-    Log
-], Product.prototype, "title", void 0);
-__decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(0, Log4)
-], Product.prototype, "getPriceWithTax", null);
+    AutoBind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector('button');
+button.addEventListener('click', p.showMessage);
--- src/app.ts
+++ src/app.ts
@@ -1,83 +1,116 @@
-const Logger = (logString: string) => {
-    return (constructor: Function) => {
-        console.log(logString);
-        console.log(constructor);
-    }
-}
+// const Logger = (logString: string) => {
+//     return (constructor: Function) => {
+//         console.log(logString);
+//         console.log(constructor);
+//     }
+// }
 
-const WithTemplate = (template: string, hookId: string) =>{
-    return <T extends {new(...args: any[]): { name: string}}>(originalConstructor: T) => {
-        return class extends originalConstructor {
-            constructor(..._: any[]) {
-                super();
-                const element = document.getElementById(hookId);
-                if (element) {
-                    element.innerHTML = template;
-                    element.querySelector('h1')!.textContent = this.name;
-                }
-            }
-        }
-    }
-}
+// const WithTemplate = (template: string, hookId: string) =>{
+//     return <T extends {new(...args: any[]): { name: string}}>(originalConstructor: T) => {
+//         return class extends originalConstructor {
+//             constructor(..._: any[]) {
+//                 super();
+//                 const element = document.getElementById(hookId);
+//                 if (element) {
+//                     element.innerHTML = template;
+//                     element.querySelector('h1')!.textContent = this.name;
+//                 }
+//             }
+//         }
+//     }
+// }
 
-@Logger('Logging')
-@WithTemplate('<h1>My person object</h1>', 'app')
-class Person {
-    name = 'JD';
+// @Logger('Logging')
+// @WithTemplate('<h1>My person object</h1>', 'app')
+// class Person {
+//     name = 'JD';
 
-    constructor() {
-        console.log('Started!');
-    }
-}
+//     constructor() {
+//         console.log('Started!');
+//     }
+// }
 
-// const person = new Person();
-// console.log("person", person);
+// // const person = new Person();
+// // console.log("person", person);
 
-const Log = (target: any, propertyName: string | Symbol) => {
-    console.log('Property decorator!');
-    console.log(target, propertyName);
-}
+// const Log = (target: any, propertyName: string | Symbol) => {
+//     console.log('Property decorator!');
+//     console.log(target, propertyName);
+// }
 
-const Log2 = (target: any, propertyName: string | Symbol, descriptor: PropertyDescriptor) => {
-    console.log('Accessor decorator!');
-    console.log('target', target);
-    console.log('propertyName', propertyName);
+// const Log2 = (target: any, propertyName: string | Symbol, descriptor: PropertyDescriptor) => {
+//     console.log('Accessor decorator!');
+//     console.log('target', target);
+//     console.log('propertyName', propertyName);
+//     console.log('descriptor', descriptor);
+// }
+
+// const Log3 = (target: any, name: string | Symbol, descriptor: PropertyDescriptor): PropertyDescriptor => {
+//     console.log('Method decorator!');
+//     console.log('target', target);
+//     console.log('name', name);
+//     console.log('descriptor', descriptor);
+
+//     return {
+
+//     }
+// }
+
+// const Log4 = (target: any, name: string | Symbol, position: number) => {
+//     console.log('Parameter decorator!');
+//     console.log('target', target);
+//     console.log('name', name);
+//     console.log('position', position);
+// }
+
+// class Product {
+//     @Log
+//     title: string;
+//     private _price: number;
+
+//     constructor(t: string, p: number) {
+//         this.title = t;
+//         this._price = p;
+//     }
+
+//     @Log2
+//     set price(val: number) {
+//         if (val > 0) {
+//             this._price = val;
+//         }
+//     }
+
+//     @Log3
+//     getPriceWithTax (@Log4 tax: number) {
+//         return this._price * (1 + tax);
+//     }
+// }
+
+const AutoBind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
     console.log('descriptor', descriptor);
+
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFunction = originalMethod.bind(this);
+            return boundFunction;
+        },
+    };
+    return adjustedDescriptor;
 }
 
-const Log3 = (target: any, name: string | Symbol, descriptor: PropertyDescriptor) => {
-    console.log('Method decorator!');
-    console.log('target', target);
-    console.log('name', name);
-    console.log('descriptor', descriptor);
-}
+class Printer {
+    message = 'This works!';
 
-const Log4 = (target: any, name: string | Symbol, position: number) => {
-    console.log('Parameter decorator!');
-    console.log('target', target);
-    console.log('name', name);
-    console.log('position', position);
-}
-
-class Product {
-    @Log
-    title: string;
-    private _price: number;
-
-    constructor(t: string, p: number) {
-        this.title = t;
-        this._price = p;
-    }
-
-    @Log2
-    set price(val: number) {
-        if (val > 0) {
-            this._price = val;
-        }
-    }
-
-    @Log3
-    getPriceWithTax (@Log4 tax: number) {
-        return this._price * (1 + tax);
+    @AutoBind
+    showMessage() {
+        console.log(this.message);
     }
 }
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
